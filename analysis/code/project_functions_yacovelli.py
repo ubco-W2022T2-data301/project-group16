b114b41990ae854a4d020a8e25381f8f9b40a8aa
Robert Yacovelli <ryacovel@student.ubc.ca>
2023-04-05 09:54:07 -0700

    Add methods to method chains

--- analysis/code/project_functions_yacovelli.py
+++ analysis/code/project_functions_yacovelli.py
@@ -3,16 +3,10 @@ import numpy as np
 
 def load_and_process(url_or_path_to_csv_file):
 
-    # Method Chain 1 (Load data and deal with missing data)
+    # Method Chain 1 (I deal with the missing data and incomplete data in my analysis)
     df1 = (
           pd.read_csv(url_or_path_to_csv_file)
-      )
-
-    # Method Chain 2 (Create new columns, drop others, and do processing)
-
-    df2 = (
-          df1
-          .rename(columns = {
+           .rename(columns = {
     'x1': 'x1 (meal cheap)',
     'x2': 'x2 (meal mid-range)',
     'x3': 'x3 (McMeal)',
@@ -69,40 +63,29 @@ def load_and_process(url_or_path_to_csv_file):
     'x54': 'x54 (average monthly net salary)',
     'x55': 'x55 (mortgage interest rate)',
 }) 
-)
-    
-    df2 = df2[[
-    'city', 'country', 
+      )
 
-    'x3 (McMeal)', #McDonalds
-
-    #Groceries
-    'x9 (milk)','x10 (bread)', 'x11 (rice)', 'x12 (eggs)', 'x13 (cheese local)',
-    'x14 (chicken fillets)', 'x15 (beef round)', 'x16 (apples)',
-    'x17 (bananas)', 'x18 (oranges)', 'x19 (tomatoes)', 'x20 (potatoes)',
-    'x21 (onions)', 'x22 (lettuce)', 'x23 (water 1.5L)', 
-
-    #Transport
-    'x28 (One-Way ticket transit)', 'x29 (monthly pass transit)',
-    'x30 (taxi start)', 'x31 (taxi 1km)', 'x32 (Taxi 1hr wait)',
-    'x33 (Gas 1L)', 
-    "x34 (Volkswagen Golf)", "x35 (Toyota Corolla Sedan)", 
+    # Method Chain 2 (Create new columns, drop others, and do processing)
+    column_order = ['x54 (average monthly net salary)'] + [col for col in df1.columns if col != 'x54 (average monthly net salary)']
+    columns_to_keep = ['city', 'country', 'x3 (McMeal)', 'x9 (milk)', 'x10 (bread)', 'x11 (rice)', 'x12 (eggs)', 'x13 (cheese local)',
+                        'x14 (chicken fillets)', 'x15 (beef round)', 'x16 (apples)', 'x17 (bananas)', 'x18 (oranges)', 'x19 (tomatoes)', 
+                        'x20 (potatoes)', 'x21 (onions)', 'x22 (lettuce)', 'x23 (water 1.5L)', 'x28 (One-Way ticket transit)',
+                          'x29 (monthly pass transit)', 'x30 (taxi start)', 'x31 (taxi 1km)', 'x32 (Taxi 1hr wait)', 'x33 (Gas 1L)',
+                            "x34 (Volkswagen Golf)", "x35 (Toyota Corolla Sedan)", 'x36 (basic utilities)', 'x48 (apartment 1BR city center)',
+                             'x49 (apartment 1BR outside center)', 'x50 (apartment 3BR city center)', 'x51 (apartment 3BR outside center)', 
+                             'x52 (price per sqm city center)', 'x53 (price per sqm outside center)', 'x54 (average monthly net salary)',  'data_quality']
+    columns_to_drop = [col for col in df1.columns if col not in columns_to_keep]
    
-    #Basic
-    'x36 (basic utilities)',
-
-    #Shelter
-    'x48 (apartment 1BR city center)',
-    'x49 (apartment 1BR outside center)', 'x50 (apartment 3BR city center)',
-    'x51 (apartment 3BR outside center)', 'x52 (price per sqm city center)',
-    'x53 (price per sqm outside center)',
-
-    #Monthly net salary (after tax)
-    'x54 (average monthly net salary)',  
-
-    'data_quality']].copy()
-
-    # Make sure to return the latest dataframe
+   
+    df2 = (
+          df1 
+          .reindex(columns=column_order)
+          .drop(columns=columns_to_drop)
+          .replace('Unknown', np.nan)
+          .assign(country_code=lambda x: x['country'].str[:2].str.upper())
+          .assign(yearly_salary=lambda x: x['x54 (average monthly net salary)'] * 12)
+          .assign(city_outsidecity_apartmentdiff=lambda x: x.apply(lambda row: row['x48 (apartment 1BR city center)'] - row['x49 (apartment 1BR outside center)'], axis=1))
+)
     return df2 
 
 
